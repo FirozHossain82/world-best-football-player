0233b5c5cfac6cf3310fd88145c1895629cc16aa
FirozHossain82 <firozhossain295@gmail.com>
2022-10-01 20:32:31 +0600

    player expence section

--- js/app.js
+++ js/app.js
@@ -28,3 +28,72 @@ function displayPlayers() {
     cartContainer.appendChild(li);
   }
 }
+
+/**
+ document
+  .getElementById("calculate-button")
+  .addEventListener("click", function () {
+    const amountField = document.getElementById("per-player-input");
+    const perPlayerAmountString = amountField.value;
+    const perPlayerAmount = parseInt(perPlayerAmountString);
+    const playerTotalExpense = Number(cart * length + perPlayerAmount);
+    // console.log(playerTotalExpense);
+    const playerExpenseElement = document.getElementById("player-expenses");
+    const playerExpenseTotalElementString = playerExpenseElement.innerText;
+    const playerExpenseTotalElement = parseInt(playerExpenseTotalElementString);
+
+    const playerExpenseTotal = playerExpenseTotalElement + playerTotalExpense;
+    // console.log(playerExpenseTotal);
+
+    // playerExpenseElement.innerText = playerExpenseTotal;
+
+    // amountField.value = "";
+  });
+ */
+
+function getInputFieldValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputFieldValueString = inputField.value;
+  const inputFieldValue = parseInt(inputFieldValueString);
+  inputField.value = "";
+  return inputFieldValue;
+}
+
+function getTextElementValueById(elementId) {
+  const textElement = document.getElementById(elementId);
+  const textElementAmountString = textElement.innerText;
+  const textElementAmount = parseInt(textElementAmountString);
+  return textElementAmount;
+}
+
+function setTextElementValueById(elementId, newValue) {
+  const textElement = document.getElementById(elementId);
+  textElement.innerText = newValue;
+}
+
+document
+  .getElementById("calculate-button")
+  .addEventListener("click", function () {
+    const perPlayerInputAmount = getInputFieldValueById("per-player-input");
+    if (perPlayerInputAmount == "number" || perPlayerInputAmount > 0) {
+      const playerTotalExp = Number(cart.length * perPlayerInputAmount);
+      const playerExpenseTotal = getTextElementValueById("player-expenses");
+      setTextElementValueById("player-expenses", playerTotalExp);
+    }
+  });
+
+// document.getElementById('calculate-total-button').addEventListener('click', function () {
+//     const managerInputField = document.getElementById('manager-input')
+//     const managerInputAmount = managerInputField.value
+
+//     const coachInputField = document.getElementById('coach-input')
+//     const coachInputAmount = coachInputField.value
+
+//     const playerExpenseField = document.getElementById('player-expenses')
+//     const playerExpenseTotal = playerExpenseField.innerText
+
+//     const totalFiled = document.getElementById('total')
+//     const totalAmount = totalFiled.innerText
+
+//     const total = Number(totalAmount) + Number(playerExpenseTotal) + Number(managerInputAmount) + Number(coachInputAmount)
+//     totalFiled.innerText = total
